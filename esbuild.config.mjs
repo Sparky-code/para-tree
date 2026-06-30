7026034c7d5f15d0eb8991519e93188251ab4986
Devin Krizwold <dkrizwold@berkeley.edu>
2026-06-30 15:18:07 -0700

    updates per release review notes

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,12 +1,12 @@
 import esbuild from "esbuild";
-import builtins from "builtin-modules";
+import { builtinModules } from "node:module";
 
 const prod = process.argv[2] === "production";
 
 const options = {
   entryPoints: ["src/main.ts"],
   bundle: true,
-  external: ["obsidian", "electron", ...builtins],
+  external: ["obsidian", "electron", ...builtinModules, ...builtinModules.map((m) => `node:${m}`)],
   format: "cjs",
   target: "es2018",
   platform: "browser",
